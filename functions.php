4ce53a3aab464300dcafa59df4d182c76059a121
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-06-10 15:31:11 +0530

    update the blog

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php
+
 add_theme_support('title-tag');
-function rrcwebsite_enqueue_styles()
-{
+add_theme_support('post-thumbnails');
+
+function rrcwebsite_enqueue_styles() {
     wp_enqueue_style(
         'rrcwebsite-style',
         get_stylesheet_uri(),
         array(),
-        '1.0'
+        filemtime(get_stylesheet_directory() . '/style.css')
     );
 }
 add_action('wp_enqueue_scripts', 'rrcwebsite_enqueue_styles');
